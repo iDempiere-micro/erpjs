ae5b3c47650cd33d1254c85c5527cfb385c4543b
davidpodhola <david@podhola.net>
2021-02-02 19:14:37 +0100

    deploy admin fixed

--- clients/admin/build_deploy_dokku_web.sh
+++ clients/admin/build_deploy_dokku_web.sh
@@ -5,7 +5,7 @@ rm -rf build
 API_BASE_URL=https://erpjs-api.app.naseukoly.cz/graphql \
 KEYCLOAK_BASE_URL=https://keycloak.app.naseukoly.cz/auth \
 KEYCLOAK_REALM=erpjs \
-URL=https://erpjs.app.naseukoly.cz/ \
+URL=https://erpjs-admin.app.naseukoly.cz/ \
  yarn build
 
 cd build
