0197e06101f57e1d5d72dd7c234b3d1fa2885b0b
David Podhola <david.podhola@naseukoly.cz>
2021-04-08 10:37:28 +0200

    autocomplete='disabled' everywhere (#49)

--- clients/admin/src/components/add-sales-invoice/NarrationColumn.svelte
+++ clients/admin/src/components/add-sales-invoice/NarrationColumn.svelte
@@ -10,6 +10,7 @@
         <input
             type="text"
             class="mt-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-red-300 rounded-md"
+            autocomplete="disabled"
             bind:value
             on:blur|preventDefault={() => (row.narration = value)}
         />
