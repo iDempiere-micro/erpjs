8870c88de6033feb15b392e31dc4cc4d0e903d48
David Podhola <david.podhola@naseukoly.cz>
2021-04-16 22:15:46 +0200

    Show the "Approve Invoice" Button only when the invoice is in draft (#71)

--- clients/admin/src/pages/SalesInvoiceDetail.svelte
+++ clients/admin/src/pages/SalesInvoiceDetail.svelte
@@ -72,15 +72,17 @@
             {$_('status.error')}
         {/if}
 
-        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-            <button
-                type="submit"
-                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                on:click|preventDefault={confirmSalesInvoiceClick}
-            >
-                {$_('page.salesInvoices.detail.confirm')}
-            </button>
-        </div>
+        {#if $salesInvoice?.data?.salesInvoice.isDraft}
+            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                <button
+                    type="submit"
+                    class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    on:click|preventDefault={confirmSalesInvoiceClick}
+                >
+                    {$_('page.salesInvoices.detail.confirm')}
+                </button>
+            </div>
+        {/if}
     </span>
 </Page>
 
