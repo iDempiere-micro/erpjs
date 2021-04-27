ad41cb8e399c63659337ef3d7ffa6f5e9872d610
davidpodhola <david@podhola.net>
2021-04-27 06:47:40 +0200

    remove WIP as we need to merge

--- clients/admin/src/components/customer-detail/CustomerDetailPageHeader.svelte
+++ clients/admin/src/components/customer-detail/CustomerDetailPageHeader.svelte
@@ -181,8 +181,7 @@
                 <button
                     type="button"
                     class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    on:click|preventDefault={
- () => {
+                    on:click|preventDefault={() => {
                         push(urls.customer.edit, id);
                     }}
                 >
