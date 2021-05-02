9eefc03b1031f7a50687c2e4566fd305e9a40bc3
davidpodhola <david@podhola.net>
2021-05-02 13:07:21 +0200

    detail is a separate component

--- clients/admin/src/pages/AccountingSchemeDetail.svelte
+++ clients/admin/src/pages/AccountingSchemeDetail.svelte
@@ -4,6 +4,7 @@
     import { _ } from 'svelte-i18n';
     import Page from '../Page.svelte';
     import Button from '../dsl/Button.svelte';
+    import AccountingSchemeDetail from '../components/accountingScheme-detail/AccountingSchemeDetail.svelte';
 
     export let params: any = {};
     const id = parseInt('' + params.id);
@@ -27,26 +28,7 @@
                         {$_('page.accountingSchemes.detail.info')}
                     </h3>
                 </div>
-                <div class="border-t border-gray-200">
-                    <dl>
-                        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">
-                                {$_('page.accountingSchemes.detail.displayName')}
-                            </dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                {$store.data.displayName}
-                            </dd>
-                        </div>
-                        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">
-                                {$_('page.accountingSchemes.detail.currency')}
-                            </dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                {$store.data.currency.displayName}
-                            </dd>
-                        </div>
-                    </dl>
-                </div>
+                <AccountingSchemeDetail data={$store.data} />
                 <div class="px-4 py-3 bg-white text-right sm:px-6">
                     <Button
                         label={$_('actions.edit')}
--- clients/admin/src/stories/AccountingSchemeDetail.stories.svelte
+++ clients/admin/src/stories/AccountingSchemeDetail.stories.svelte
@@ -1,8 +1,9 @@
 <script>
     import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
-    import AccountingSchemeDetail from '../pages/AccountingSchemeDetail.svelte';
+    import AccountingSchemeDetail from '../components/accountingScheme-detail/AccountingSchemeDetail.svelte';
     import { apollo } from '../lib/support/apollo';
     import { setupLocales } from '../i18n';
+    import { mock1 } from '../../build/dist/lib/queries/accountingScheme';
 
     const client = apollo(true);
 
@@ -16,7 +17,7 @@
 />
 
 <Template let:args>
-    <AccountingSchemeDetail {...args} />
+    <AccountingSchemeDetail data={mock1.data.accountingScheme} {...args} />
 </Template>
 
 <Story name="Detail" />
