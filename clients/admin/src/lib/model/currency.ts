2a0e3a3754ddacf9c74d5c661e91ec1a2d7eaf39
davidpodhola <david@podhola.net>
2021-05-03 06:41:28 +0200

    currency started

---
+++ clients/admin/src/lib/model/currency.ts
@@ -0,0 +1,4 @@
+import type { CurrencyDetailPartsFragment, CurrencyListPartsFragment } from '../../generated/graphql';
+
+export type CurrencyDetail = CurrencyDetailPartsFragment;
+export type CurrencyRow = CurrencyListPartsFragment;
