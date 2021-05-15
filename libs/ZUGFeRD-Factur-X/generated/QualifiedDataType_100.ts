19ad7e6a140a23cc7a142a4f5fb797e00142084c
davidpodhola <david@podhola.net>
2021-05-15 19:13:02 +0200

    generated MINIMAL classes added

---
+++ libs/ZUGFeRD-Factur-X/generated/QualifiedDataType_100.ts
@@ -0,0 +1,31 @@
+/***********
+generated template classes for ./FACTUR-X_MINIMUM_urn_un_unece_uncefact_data_standard_QualifiedDataType_100.xsd 5/15/2021, 14:41:45
+***********/
+
+
+
+
+
+export class FACTUR_X_MINIMUM_urn_un_unece_uncefact_data_standard_QualifiedDataType_100 {
+    public constructor(props?: FACTUR_X_MINIMUM_urn_un_unece_uncefact_data_standard_QualifiedDataType_100) {
+        this["@class"] = ".FACTUR_X_MINIMUM_urn_un_unece_uncefact_data_standard_QualifiedDataType_100";
+    }
+}
+
+export class CountryIDType {
+    public constructor(props?: CountryIDType) {
+        this["@class"] = ".CountryIDType";
+    }
+}
+
+export class CurrencyCodeType {
+    public constructor(props?: CurrencyCodeType) {
+        this["@class"] = ".CurrencyCodeType";
+    }
+}
+
+export class DocumentCodeType {
+    public constructor(props?: DocumentCodeType) {
+        this["@class"] = ".DocumentCodeType";
+    }
+}
---
+++ libs/ZUGFeRD-Factur-X/generated/UnqualifiedDataType_100.ts
@@ -0,0 +1,43 @@
+/***********
+generated template classes for ./FACTUR-X_MINIMUM_urn_un_unece_uncefact_data_standard_UnqualifiedDataType_100.xsd 5/15/2021, 14:41:45
+***********/
+
+
+
+
+
+export class FACTUR_X_MINIMUM_urn_un_unece_uncefact_data_standard_UnqualifiedDataType_100 {
+    public constructor(props?: FACTUR_X_MINIMUM_urn_un_unece_uncefact_data_standard_UnqualifiedDataType_100) {
+        this["@class"] = ".FACTUR_X_MINIMUM_urn_un_unece_uncefact_data_standard_UnqualifiedDataType_100";
+    }
+}
+
+export class AmountType {
+    public constructor(props?: AmountType) {
+        this["@class"] = ".AmountType";
+    }
+}
+
+export class CodeType {
+    public constructor(props?: CodeType) {
+        this["@class"] = ".CodeType";
+    }
+}
+
+export class DateTimeType {
+    public constructor(props?: DateTimeType) {
+        this["@class"] = ".DateTimeType";
+    }
+}
+
+export class IDType {
+    public constructor(props?: IDType) {
+        this["@class"] = ".IDType";
+    }
+}
+
+export class TextType {
+    public constructor(props?: TextType) {
+        this["@class"] = ".TextType";
+    }
+}
