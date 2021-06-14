eb8e3ed471260e9879ceef631b7adf217aec5185
davidpodhola <david@podhola.net>
2021-06-14 16:36:03 +0200

    authentication in the base-app

--- clients/web/base-app/index.ts
+++ clients/web/base-app/index.ts
@@ -59,7 +59,7 @@ app.get("/", async (req: any, res: any) => {
   incoming.view.title = "Home Page";
 
   const result = `<div>
-    ${content.join()}
+    ${content.join('')}
   </div>
   <script>
     console.log('*** main app loaded');
@@ -80,7 +80,37 @@ app.get("/", async (req: any, res: any) => {
         }
     };
     
-    document.addEventListener("DOMContentLoaded", moveApplications);
+    export const authenticate = (callback) => {
+    if (!process.env.MOCK && !process.env.FAKE_TOKEN && !window.token) {
+        const keycloak = Keycloak({
+            url: process.env.KEYCLOAK_BASE_URL,
+            realm: process.env.KEYCLOAK_REALM || 'erpjs',
+            clientId: 'erpjs',
+            flow: 'implicit',
+        });
+        keycloak
+            .init({})
+            .then(function (authenticated) {
+                if (!authenticated) {
+                    keycloak.login({
+                        redirectUri: process.env.URL,
+                    });
+                } else {
+                    const { token } = keycloak;
+                    if (token) {
+                        // save the token
+                        window.token = token;
+                        callback();
+                    }
+                }
+            })
+            .catch(function (f) {
+                console.error('Keycloak login failed', f);
+            });
+        }
+    };
+    
+    document.addEventListener("DOMContentLoaded", ()=>authenticate(moveApplications));
   </script>
   `;
 
