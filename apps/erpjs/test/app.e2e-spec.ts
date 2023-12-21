c6efc3ad6fd5f914e3be48d36f3f7411a7b048d1
David Podhola <david.podhola@headout.com>
2023-12-21 09:59:20 +0100

    can start till pg

--- apps/erpjs/test/app.e2e-spec.ts
+++ apps/erpjs/test/app.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from './../src/app/app.module';
 
-describe('AppController (e2e)', () => {
+describe('ApiController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
